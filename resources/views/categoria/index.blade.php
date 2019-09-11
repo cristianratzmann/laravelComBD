853aa7a57a2dc5af10dabffe9d148c7275bbc84e
Aluno <Aluno@SN-304591>
2019-09-10 21:48:32 -0300

    ProjetoAtualizado

--- resources/views/categoria/index.blade.php
+++ resources/views/categoria/index.blade.php
@@ -8,6 +8,16 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <script>
+            
+    window.onload = function(){
+        @if(session()->get('msg'))
+            alert("{{session()->get('msg')}}");
+        @endif
+        
+        };
+        </script>
+        
         
     </head>
     <body>
@@ -25,7 +35,17 @@
                 <tr>
                     <td>{{$c->codcat}}</td>
                     <td>{{$c->nomcat}}</td>
-                    <td>Editar | Excluir</td>
+                    <td>
+                        
+                        <button onclick="location.href='{{route('categoria.edit', $c->codcat)}}'" type="button">Editar</button>
+                        
+                        <form action="{{route('categoria.destroy', $c->codcat)}}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Excluir</button>
+                        </form>
+                    
+                    </td>
                 </tr>
                 @endforeach
             </tbody>
